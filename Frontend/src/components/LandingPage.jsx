d5bbca9f4204f8d2acd7459807adedce14137490
Pratik <pratik.chavhan.77@gmail.com>
2024-02-22 14:23:41 +0530

    project frontend

---
+++ Frontend/src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const LandingPage = () => {
+    return (
+
+        <div>
+
+            <img width={'98%'} height={'900px'} src="https://ainal.me/wp-content/uploads/2020/10/Hire-Freelancer.jpg" alt="error" />
+        </div>
+    )
+};
+export default LandingPage
